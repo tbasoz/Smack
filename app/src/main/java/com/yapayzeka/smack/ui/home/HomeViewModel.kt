3642a52d95588403331847b3d8f08137c84cf165
Tekin BASOZ <tekin@yapayzeka.com>
2020-08-01 19:21:45 +0300

    lesson-02

--- app/src/main/java/com/yapayzeka/smack/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/yapayzeka/smack/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Please Log In"
     }
     val text: LiveData<String> = _text
 }
